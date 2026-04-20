469c415c2b26d05e73b905315bff0ff028aaeddf
Filip <filipnogacki@gmail.com>
2026-04-21 00:56:35 +0200

    docs: add README with setup instructions and API endpoints

--- MelodiaBackend/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post("/auth/register", registerUser);
-router.post("/auth/login", loginUser);
-
-export default router;
--- MelodiaBackend/src/routes/search.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { searchSongs } from "../controllers/search.controller";
-
-const router = Router();
-
-router.get("/search", searchSongs);
-
-export default router;
